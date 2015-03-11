618942656201cc5ba54eded00017f6da16b63062
codelogicws <russ@codelogic.ws>
2015-03-11 00:08:20 -0700

    First Commit

---
+++ app/src/androidTest/java/notepad/codelogic/ws/notepad/ApplicationTest.java
@@ -0,0 +1,13 @@
+package notepad.codelogic.ws.notepad;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
